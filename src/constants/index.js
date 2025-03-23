85fa9b70e6276468c57adc71e2b806e5ee843adc
Mateus <46897119+rochinh-a@users.noreply.github.com>
2025-03-23 01:03:02 -0300

    logo changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -39,9 +39,9 @@ export const navLinks = [
 
 export const services = [
   { title: "Python", icon: python },
-  { name: "git", icon: git },
-  { name: "React JS", icon: reactjs },
-  { name: "Node JS", icon: nodejs },
+  { title: "git", icon: git },
+  { title: "React JS", icon: reactjs },
+  { title: "Node JS", icon: nodejs },
 ];
 
 export const technologies = [
@@ -57,38 +57,15 @@ export const technologies = [
 
 export const experiences = [
   {
-    title: "AI/ML Intern",
-    company_name: "EduSkill Foundation | AWS Academy | AICTE",
+    title: "Data Scientist",
+    company_name: "Coursera | IBM",
     icon: eduskill,
     iconBg: "#161329",
-    date: "Sep 2023 - Nov 2023",
+    date: "Sep 2020 - Nov 2020",
     points: [
-      "Acquired hands-on knowledge of AWS Cloud Foundation, delving into cloud infrastructure, services, and deployment.",
-      "also gaining a solid foundation in Machine Learning, covering topics like algorithms, data analysis, and model building.",
-    ],
-  },
-  {
-    title: "Mathwork Ai Virtual Intern",
-    company_name: "Mathwork | AICTE",
-    icon: mathwork,
-    iconBg: "#161329",
-    date: "May 2023 - Sep 2023",
-    points: [
-      "Completed virtual internship, gaining a strong foundation in MATLAB, including data analysis and processing.",
-      "Acquired practical skills in image and signal processing, including segmentation, batch processing, and spectral analysis.",
-      "Developed expertise in machine learning models for clustering, classification, and regression, and customized deep learning techniques for image classification.",
-    ],
-  },
-  {
-    title: "Artificial Intelligence Intern",
-    company_name: "Edunet Foundation | IBM SkillsBuild | AICTE",
-    icon: edunet,
-    iconBg: "#161329",
-    date: "June 2023 - July 2023",
-    points: [
-      "Engineered a comprehensive Mental Health Fitness Tracker ML model utilizing Python and scikit-learn.",
-      "Maximized the model's performance by refining model parameters and employing ensemble methods, yielding an outstanding accuracy percentage of 98.50%.",
-      "Leveraged 12 regression algorithms to attain precise outcomes in analyzing and predicting mental fitness levels across 150+ countries.",
+      "Adquired a strong foundation in Data Science, covering topics like data analysis, visualization, and machine learning.",
+      "Completed hands-on projects to gain practical experience in data analysis and machine learning.",
+      "Also gaining a solid foundation in Machine Learning, covering topics like algorithms, data analysis, and model building.",
     ],
   },
 ];
